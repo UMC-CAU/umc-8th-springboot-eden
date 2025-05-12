854a401ab718a0a9a8ce1054f1aae5b9e2a67050
yc3697 <youngchan9683@gmail.com>
2025-05-12 21:49:38 +0900

    feat: API 응답에 들어갈 code와 message의 형식 관련 클래스 이해용 설명 추가

--- src/main/java/umc/study/apiPayload/code/BaseErrorCode.java
+++ src/main/java/umc/study/apiPayload/code/BaseErrorCode.java
@@ -1,5 +1,6 @@
 package umc.study.apiPayload.code;
 
+// Status를 구체화하는 enum class에서 아래의 메소드들의 구현을 강제하기 위한 인터페이스
 public interface BaseErrorCode {
 
     ErrorReasonDTO getReason();
--- src/main/java/umc/study/apiPayload/code/status/SuccessStatus.java
+++ src/main/java/umc/study/apiPayload/code/status/SuccessStatus.java
@@ -11,14 +11,14 @@ import umc.study.apiPayload.code.ReasonDTO;
 public enum SuccessStatus implements BaseCode {
 
     // 일반적인 응답
-    _OK(HttpStatus.OK, "COMMON200", "성공입니다.");
+    _OK(HttpStatus.OK, "COMMON200", "성공입니다.");  // 멤버인 httpStatus, code, message에 대응하는 것으로 enum 상수 정의
 
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
 
     @Override
-    public ReasonDTO getReason() {
+    public ReasonDTO getReason() {  // httpStatus 제외...내부 처리용
         return ReasonDTO.builder()
                 .message(message)
                 .code(code)
@@ -27,7 +27,7 @@ public enum SuccessStatus implements BaseCode {
     }
 
     @Override
-    public ReasonDTO getReasonHttpStatus() {
+    public ReasonDTO getReasonHttpStatus() {    // 외부 응답용
         return ReasonDTO.builder()
                 .message(message)
                 .code(code)
@@ -36,4 +36,9 @@ public enum SuccessStatus implements BaseCode {
                 .build()
                 ;
     }
+
+    /*
+    * 지금은 두 메소드가 거의 비슷해서 구분이 무의미한 것 같지만,
+    * 나중에 확장 시 구분이 필요할 수 있어 책임 분리 위한 것
+    * */
 }
