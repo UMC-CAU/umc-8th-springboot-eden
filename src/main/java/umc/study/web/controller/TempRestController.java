a31bb70e6835a32fd099adf2144a703cdb588977
yc3697 <youngchan9683@gmail.com>
2025-05-12 23:08:18 +0900

    feat: 에러 핸들러-exceptionAPI 완성

--- src/main/java/umc/study/web/controller/TempRestController.java
+++ src/main/java/umc/study/web/controller/TempRestController.java
@@ -7,12 +7,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import umc.study.apiPayload.ApiResponse;
 import umc.study.converter.TempConverter;
+import umc.study.service.TempService.TempQueryService;
 import umc.study.web.dto.TempResponse;
 
 @RestController     // 객체나 문자열을 바로 Response body로 반환하게 해준다
 @RequestMapping("/temp")    // 이 컨트롤러 내의 모든 URL 앞에 공통 경로 "/temp"를 붙이겠다
 @RequiredArgsConstructor
 public class TempRestController {
+
+    private final TempQueryService tempQueryService;
 
     @GetMapping("/test")    // GET 요청을 "/test" 경로로 받을 때 실행되는 메소드임을 표현
     public ApiResponse<TempResponse.TempTestDTO> testAPI(){
@@ -22,7 +25,7 @@ public class TempRestController {
 
     @GetMapping("/exception")
     public ApiResponse<TempResponse.TempExceptionDTO> exceptionAPI(@RequestParam Integer flag){ // queryString 받아오는 방법
-
-        return null;
+        tempQueryService.CheckFlag(flag);
+        return ApiResponse.onSuccess(TempConverter.toTempExceptionDTO(flag));
     }
 }
