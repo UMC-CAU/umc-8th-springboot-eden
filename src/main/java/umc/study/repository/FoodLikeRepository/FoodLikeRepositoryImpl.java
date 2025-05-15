8a1de9d80cb67dbe1a43fdbcdd9b2c67871bf8bf
yc3697 <youngchan9683@gmail.com>
2025-05-15 23:53:12 +0900

    feat: 음식 카테고리 repository 생성 #4

---
+++ src/main/java/umc/study/repository/FoodLikeRepository/FoodLikeRepositoryImpl.java
@@ -0,0 +1,4 @@
+package umc.study.repository.FoodLikeRepository;
+
+public class FoodLikeRepositoryImpl {
+}
--- src/main/java/umc/study/web/dto/UserRequestDTO.java
+++ src/main/java/umc/study/web/dto/UserRequestDTO.java
@@ -16,7 +16,8 @@ public class UserRequestDTO {
         Integer birthDay;
         String address;
         String specAddress;
-        List<Long> preferCategory;
+        List<Long> preferCategory;  // 카테고리 선택 화면 렌더링 시 음식 카테고리를 조회하는 API를 호출하고,
+                                    // 그 API의 결과에서 음식 카테고리의 id값을 프론트엔드가 넘겨준다는 것을 전제로 한 것
     }
 
 }
