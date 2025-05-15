f0e84ee28915e46c562356ff9d6b40b7639b7b68
yc3697 <youngchan9683@gmail.com>
2025-05-16 00:04:05 +0900

    feat: 음식 카테고리 converter 생성 #4

--- src/main/java/umc/study/service/UserService/UserCommandServiceImpl.java
+++ src/main/java/umc/study/service/UserService/UserCommandServiceImpl.java
@@ -2,10 +2,17 @@ package umc.study.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import umc.study.apiPayload.code.status.ErrorStatus;
+import umc.study.apiPayload.exception.handler.FoodLikeHandler;
 import umc.study.converter.UserConverter;
+import umc.study.domain.FoodLike;
 import umc.study.domain.User;
+import umc.study.repository.FoodLikeRepository.FoodLikeRepository;
 import umc.study.repository.UserRepository.UserRepository;
 import umc.study.web.dto.UserRequestDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -13,11 +20,31 @@ public class UserCommandServiceImpl implements UserCommandService {
 
     private final UserRepository userRepository;
 
+    private final FoodLikeRepository foodLikeRepository;
+
     @Override
     public User joinUser(UserRequestDTO.JoinDto request){
 
         User newUser = UserConverter.toUser(request);
+        List<FoodLike> foodLikeList = request.getPreferCategory().stream()
+                .map(category -> {
+            return foodLikeRepository.findById(category).orElseThrow(() -> new FoodLikeHandler(ErrorStatus.FOOD_CATEGORY_NOT_FOUND));
+        }).collect(Collectors.toList());
 
         return null;
+
+        /*
+        * Member newMember = MemberConverter.toMember(request);
+        List<FoodCategory> foodCategoryList = request.getPreferCategory().stream()
+                .map(category -> {
+                    return foodCategoryRepository.findById(category).orElseThrow(() -> new FoodCategoryHandler(ErrorStatus.FOOD_CATEGORY_NOT_FOUND));
+                }).collect(Collectors.toList());
+
+        List<MemberPrefer> memberPreferList = MemberPreferConverter.toMemberPreferList(foodCategoryList);
+
+        memberPreferList.forEach(memberPrefer -> {memberPrefer.setMember(newMember);});
+
+        return memberRepository.save(newMember);
+        * */
     }
 }
