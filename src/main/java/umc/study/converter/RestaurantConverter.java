c0e409531d999439b4d98f169f526e17505b5457
yc3697 <youngchan9683@gmail.com>
2025-05-17 21:11:57 +0900

    feat: API_1 converter에 DTO to entity 메소드 구현(Restaurant 중심 ver.) #6

--- src/main/java/umc/study/converter/RestaurantConverter.java
+++ src/main/java/umc/study/converter/RestaurantConverter.java
@@ -1,15 +1,26 @@
 package umc.study.converter;
 
 import umc.study.domain.Restaurant;
+import umc.study.web.dto.RestaurantRequestDTO;
 import umc.study.web.dto.RestaurantResponseDTO;
 
 public class RestaurantConverter {
 
+    // 식당 객체 -> DTO
     public static RestaurantResponseDTO.addToRegionResultDTO toRestaurant(Restaurant restaurant){
         return RestaurantResponseDTO.addToRegionResultDTO.builder()
                 .name(restaurant.getName())
                 .address(restaurant.getAddress())
                 .regionId(restaurant.getRegion().getId())
+                .build();
+    }
+
+    // DTO -> 식당 객체
+    public static Restaurant toRestaurant(RestaurantRequestDTO.addToRegionDTO request){
+        return Restaurant.builder()
+                .name(request.getName())
+                .category(request.getCategory())
+                .address(request.getAddress())
                 .build();
     }
 }
