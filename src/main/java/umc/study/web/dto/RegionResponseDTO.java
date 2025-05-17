3fc81954d19629b882061ec76df682bb0f6361cb
yc3697 <youngchan9683@gmail.com>
2025-05-17 16:59:07 +0900

    feat: API_1 DTO 생성 #6

---
+++ src/main/java/umc/study/web/dto/RegionResponseDTO.java
@@ -0,0 +1,19 @@
+package umc.study.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class RegionResponseDTO {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class addRestaurantResultDTO{
+        String name;
+
+        String address;
+    }
+}
