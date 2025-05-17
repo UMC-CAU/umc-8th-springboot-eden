5c728fbb45823510d9150dfe7ece901b89f85394
yc3697 <youngchan9683@gmail.com>
2025-05-17 21:19:36 +0900

    feat: API_1 service 구현(Restaurant 중심 ver.) #6

--- src/main/java/umc/study/service/RestaurantService/RestaurantCommandService.java
+++ src/main/java/umc/study/service/RestaurantService/RestaurantCommandService.java
@@ -1,4 +1,9 @@
 package umc.study.service.RestaurantService;
 
+import umc.study.domain.Restaurant;
+import umc.study.web.dto.RestaurantRequestDTO;
+
 public interface RestaurantCommandService {
+
+    Restaurant addToRegion(RestaurantRequestDTO.addToRegionDTO request);
 }
