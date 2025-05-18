2656f2480b4b17d0d360d41d678c8ab66ecfe838
yc3697 <youngchan9683@gmail.com>
2025-05-18 11:29:39 +0900

    feat: API_2 DTO to Entity converter 구현 #6

--- src/main/java/umc/study/converter/ReviewConverter.java
+++ src/main/java/umc/study/converter/ReviewConverter.java
@@ -1,4 +1,14 @@
 package umc.study.converter;
 
+import umc.study.domain.Review;
+import umc.study.web.dto.ReviewRequestDTO;
+
 public class ReviewConverter {
+
+    public static Review toReviewEntity(ReviewRequestDTO.reviewRestaurantDTO request){
+        return Review.builder()
+                .rating(request.getRating())
+                .content(request.getContent())
+                .build();
+    }
 }
