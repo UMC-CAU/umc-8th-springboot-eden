f5be6ae308d69a5caba52be1ea7257410861855d
yc3697 <youngchan9683@gmail.com>
2025-05-18 11:25:02 +0900

    feat: API_2 controller, service, converter 생성 #6

--- src/main/java/umc/study/web/dto/ReviewRequestDTO.java
+++ src/main/java/umc/study/web/dto/ReviewRequestDTO.java
@@ -1,7 +1,10 @@
 package umc.study.web.dto;
 
+import lombok.Getter;
+
 public class ReviewRequestDTO {
 
+    @Getter
     public static class reviewRestaurantDTO {
 
         Integer rating;
--- src/main/java/umc/study/web/dto/ReviewResponseDTO.java
+++ src/main/java/umc/study/web/dto/ReviewResponseDTO.java
@@ -1,9 +1,18 @@
 package umc.study.web.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 public class ReviewResponseDTO {
 
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class reviewRestaurantResultDTO {
 
         Long reviewId;
