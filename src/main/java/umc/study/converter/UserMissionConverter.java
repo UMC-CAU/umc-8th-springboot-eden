dc6d4ca646b4594bf1ba0c6d97b1096fc2e6e089
yc3697 <youngchan9683@gmail.com>
2025-05-19 00:33:59 +0900

    feat: API_4 controller 구현 #6

--- src/main/java/umc/study/converter/UserMissionConverter.java
+++ src/main/java/umc/study/converter/UserMissionConverter.java
@@ -1,4 +1,19 @@
 package umc.study.converter;
 
+import umc.study.domain.mapping.UserMission;
+import umc.study.web.dto.UserMissionResponseDTO;
+
 public class UserMissionConverter {
+
+    public static UserMissionResponseDTO.JoinDTO toCreateUserMissionResultDTO(UserMission userMission) {
+
+        return UserMissionResponseDTO.JoinDTO.builder()
+                .missionId(userMission.getMission().getId())
+                .restaurantName(userMission.getMission().getRestaurant().getName())
+                .missionStatus(userMission.getMissionStatus())
+                .createdAt(userMission.getCreatedAt())
+                .updatedAt(userMission.getUpdatedAt())
+                .build();
+
+    }
 }
