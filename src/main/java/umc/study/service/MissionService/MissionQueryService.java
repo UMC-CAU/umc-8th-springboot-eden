682605678583f2e023fc8a2546c2e75b309c983d
yc3697 <youngchan9683@gmail.com>
2025-05-19 14:48:07 +0900

    feat: 미션 시작 시 진행 여부 검증 구현 #6

--- src/main/java/umc/study/service/MissionService/MissionQueryService.java
+++ src/main/java/umc/study/service/MissionService/MissionQueryService.java
@@ -1,4 +1,10 @@
 package umc.study.service.MissionService;
 
+import umc.study.domain.Mission;
+
+import java.util.Optional;
+
 public interface MissionQueryService {
+
+    Optional<Mission> findById(Long id);
 }
--- src/main/java/umc/study/web/controller/UserMissionRestController.java
+++ src/main/java/umc/study/web/controller/UserMissionRestController.java
@@ -1,6 +1,7 @@
 package umc.study.web.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,18 +10,20 @@ import umc.study.apiPayload.ApiResponse;
 import umc.study.converter.UserMissionConverter;
 import umc.study.domain.mapping.UserMission;
 import umc.study.service.UserMissionService.UserMissionCommandService;
+import umc.study.validation.annotation.IngMission;
 import umc.study.web.dto.UserMissionResponseDTO;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
+@Validated
 public class UserMissionRestController {
 
     private final UserMissionCommandService userMissionCommandService;
 
     @PostMapping("/mission/{missionId}/join")
     public ApiResponse<UserMissionResponseDTO.JoinDTO> joinUserMission(
-            @PathVariable Long missionId
+           @IngMission @PathVariable Long missionId
     ) {
         UserMission userMission = userMissionCommandService.createUserMission(missionId);
 
