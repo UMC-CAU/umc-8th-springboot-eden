23b005010ee59799176acf04209e6fc814ad3584
yc3697 <youngchan9683@gmail.com>
2025-05-19 14:01:26 +0900

    feat: 리뷰 작성 시 식당 검증-repository에 대한 접근 변경 #6

--- src/main/java/umc/study/validation/validator/RestaurantsExistValidator.java
+++ src/main/java/umc/study/validation/validator/RestaurantsExistValidator.java
@@ -6,13 +6,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import umc.study.apiPayload.code.status.ErrorStatus;
 import umc.study.repository.RestaurantRepository.RestaurantRepository;
+import umc.study.service.RestaurantService.RestaurantQueryService;
 import umc.study.validation.annotation.ExistRestaurants;
 
 @Component
 @RequiredArgsConstructor
 public class RestaurantsExistValidator implements ConstraintValidator<ExistRestaurants, Long> {
 
-    private final RestaurantRepository restaurantRepository;
+    private final RestaurantQueryService restaurantQueryService;
 
     @Override
     public void initialize(ExistRestaurants constraintAnnotation) {
@@ -22,7 +23,7 @@ public class RestaurantsExistValidator implements ConstraintValidator<ExistResta
     @Override
     public boolean isValid(Long restaurantId, ConstraintValidatorContext context) {
 
-        boolean isValid = restaurantRepository.existsById(restaurantId);
+        boolean isValid = restaurantQueryService.findStore(restaurantId).isPresent();
 
         if (!isValid) {
             context.disableDefaultConstraintViolation();
