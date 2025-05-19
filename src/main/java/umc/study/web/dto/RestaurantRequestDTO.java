7924583351bb430daea7b20a8e99ec2fbb3875fa
yc3697 <youngchan9683@gmail.com>
2025-05-19 11:39:29 +0900

    feat: request DTO 유효성 검사 추가 #6

--- src/main/java/umc/study/web/dto/RestaurantRequestDTO.java
+++ src/main/java/umc/study/web/dto/RestaurantRequestDTO.java
@@ -1,5 +1,6 @@
 package umc.study.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 public class RestaurantRequestDTO {
@@ -7,10 +8,13 @@ public class RestaurantRequestDTO {
     @Getter
     public static class AddToRegionDTO {
 
+        @NotBlank(message = "이름을 입력하세요")
         String name;
 
+        @NotBlank(message = "카테고리를 입력하세요")
         String category;
 
+        @NotBlank(message = "주소를 입력하세요")
         String address;
     }
 }
