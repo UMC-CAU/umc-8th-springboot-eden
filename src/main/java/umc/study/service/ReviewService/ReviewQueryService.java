44ddc875932cec833c053b632d92edeb7e17a90a
yc3697 <youngchan9683@gmail.com>
2025-05-28 01:49:13 +0900

    feat: API_1 service #10

--- src/main/java/umc/study/service/ReviewService/ReviewQueryService.java
+++ src/main/java/umc/study/service/ReviewService/ReviewQueryService.java
@@ -1,4 +1,9 @@
 package umc.study.service.ReviewService;
 
+import org.springframework.data.domain.Page;
+import umc.study.domain.Review;
+
 public interface ReviewQueryService {
+
+    Page<Review> getReviewByPage(int number, int size);
 }
--- src/main/java/umc/study/service/ReviewService/ReviewQueryServiceImpl.java
+++ src/main/java/umc/study/service/ReviewService/ReviewQueryServiceImpl.java
@@ -1,4 +1,23 @@
 package umc.study.service.ReviewService;
 
-public class ReviewQueryServiceImpl {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import umc.study.domain.Review;
+import umc.study.domain.User;
+import umc.study.repository.ReviewRepository.ReviewRepository;
+import umc.study.repository.UserRepository.UserRepository;
+
+public class ReviewQueryServiceImpl implements ReviewQueryService {
+
+    private UserRepository userRepository;
+
+    private ReviewRepository reviewRepository;
+
+    @Override
+    public Page<Review> getReviewByPage(int number, int size) {
+
+        User myUserObj = userRepository.findById(1L).get();
+
+        return reviewRepository.findAllByUser(myUserObj, PageRequest.of(number, 10));
+    }
 }
