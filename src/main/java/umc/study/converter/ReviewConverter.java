6a7d1e66ea5d40147906d0c200d0026e8d4bdd91
yc3697 <youngchan9683@gmail.com>
2025-05-27 17:40:47 +0900

    feat: api 시그니처 생성 #8

--- src/main/java/umc/study/converter/ReviewConverter.java
+++ src/main/java/umc/study/converter/ReviewConverter.java
@@ -4,6 +4,8 @@ import umc.study.domain.Review;
 import umc.study.web.dto.ReviewRequestDTO;
 import umc.study.web.dto.ReviewResponseDTO;
 
+import java.util.List;
+
 public class ReviewConverter {
 
     public static Review toReviewEntity(ReviewRequestDTO.ReviewRestaurantDTO request){
@@ -21,5 +23,12 @@ public class ReviewConverter {
                 .createdAt(review.getCreatedAt())
                 .updatedAt(review.getUpdatedAt())
                 .build();
+    }
+
+    public static ReviewResponseDTO.ReviewPreViewDTO reviewPreViewDTO(Review review){
+        return null;
+    }
+    public static ReviewResponseDTO.ReviewPreViewListDTO reviewPreViewListDTO(List<Review> reviewList){
+        return null;
     }
 }
