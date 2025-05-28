e0ca28f766b1e8f90483d3743b8062135468e0f6
yc3697 <youngchan9683@gmail.com>
2025-05-28 14:32:36 +0900

    feat: API_2 service #10

--- src/main/java/umc/study/service/MissionService/MissionQueryServiceImpl.java
+++ src/main/java/umc/study/service/MissionService/MissionQueryServiceImpl.java
@@ -1,9 +1,12 @@
 package umc.study.service.MissionService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.study.domain.Mission;
+import umc.study.domain.Restaurant;
 import umc.study.repository.MissionRepository.MissionRepository;
 
 import java.util.Optional;
@@ -19,4 +22,11 @@ public class MissionQueryServiceImpl implements MissionQueryService {
     public Optional<Mission> findById(Long id) {
         return missionRepository.findById(id);
     }
+
+    @Override
+    public Page<Mission> getByRestaurantByPage(Restaurant restaurant, int number, int size) {
+
+        return missionRepository.findAllByRestaurant(restaurant, PageRequest.of(number, size));
+    }
+
 }
