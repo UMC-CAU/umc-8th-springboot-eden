8749343f8418b48c2508de60563abb9258974230
yc3697 <youngchan9683@gmail.com>
2025-05-28 14:16:58 +0900

    feat: API_2 DTO #10

--- src/main/java/umc/study/web/dto/MissionResponseDTO.java
+++ src/main/java/umc/study/web/dto/MissionResponseDTO.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class MissionResponseDTO {
 
@@ -27,5 +28,37 @@ public class MissionResponseDTO {
         LocalDateTime createAt;
 
         LocalDateTime updateAt;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class storeMissionDTO {
+        String info;
+
+        LocalDateTime deadLine;
+
+        BigDecimal successAmount;
+
+        BigDecimal rewardAmount;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class storeMissionListDTO {
+        List<storeMissionDTO> reviewList;
+
+        Integer listSize;
+
+        Integer totalPage;
+
+        Long totalElements;
+
+        Boolean isFirst;
+
+        Boolean isLast;
     }
 }
