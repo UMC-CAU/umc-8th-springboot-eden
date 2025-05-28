33e297b3971fd442dd6f64f93e4c7be12db7e2e4
yc3697 <youngchan9683@gmail.com>
2025-05-28 13:21:45 +0900

    feat: API_1 controller #10

--- src/main/java/umc/study/service/ReviewService/ReviewQueryServiceImpl.java
+++ src/main/java/umc/study/service/ReviewService/ReviewQueryServiceImpl.java
@@ -1,22 +1,26 @@
 package umc.study.service.ReviewService;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 import umc.study.domain.Review;
 import umc.study.domain.User;
 import umc.study.repository.ReviewRepository.ReviewRepository;
 import umc.study.repository.UserRepository.UserRepository;
 
+@Service
+@RequiredArgsConstructor
 public class ReviewQueryServiceImpl implements ReviewQueryService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    private ReviewRepository reviewRepository;
+    private final ReviewRepository reviewRepository;
 
     @Override
     public Page<Review> getReviewByPage(int number, int size) {
 
-        User myUserObj = userRepository.findById(1L).get();
+        User myUserObj = userRepository.findById(1L).orElse(null);
 
         return reviewRepository.findAllByUser(myUserObj, PageRequest.of(number, 10));
     }
