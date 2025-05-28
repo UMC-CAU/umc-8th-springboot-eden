0a8a524af9820b7c2ef32e4119e35beebc22cc49
yc3697 <youngchan9683@gmail.com>
2025-05-28 14:55:32 +0900

    feat: API_3 repository #10

--- src/main/java/umc/study/repository/MissionRepository/MissionRepository.java
+++ src/main/java/umc/study/repository/MissionRepository/MissionRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.study.domain.Mission;
 import umc.study.domain.Restaurant;
+import umc.study.domain.User;
 
 public interface MissionRepository extends JpaRepository<Mission, Long>, MissionRepositoryCustom {
     Page<Mission> findAllByRestaurant(Restaurant restaurant, PageRequest pageRequest);
+
+    Page<Mission> findAllByUser(User user, PageRequest pageRequest);
 }
